00b1d85e6ac0e21ed46118134b8598bada2613ec
YYYTTT-code <yt1172910316@163.com>
2021-03-08 15:19:04 +0800

    学习servlet

---
+++ LearnTomcat/src/com/atguigu/HelloServlet2.java
@@ -0,0 +1,19 @@
+package com.atguigu;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("get被执行");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("post被执行");
+    }
+}

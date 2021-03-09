9a06050e0ee5e5f423ecc5401c1e1ac25fccf718
YYYTTT-code <yt1172910316@163.com>
2021-03-09 16:44:43 +0800

    搭建书城项目第二阶段——基本完成

--- myBook_02/src/com/atguigu/web/RegistServlet.java
+++ myBook_02/src/com/atguigu/web/RegistServlet.java
@@ -1,6 +1,8 @@
 package com.atguigu.web;
 
 import com.atguigu.pojo.User;
+import com.atguigu.service.UserService;
+import com.atguigu.service.impl.UserServiceImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,8 +12,31 @@ import java.io.IOException;
 
 public class RegistServlet extends HttpServlet {
 
+    private UserService userService=new UserServiceImpl();
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        User user=new User();
+        String username=request.getParameter("username");
+        String password=request.getParameter("password");
+        String email =request.getParameter("email");
+        String code=request.getParameter("code");
+//        System.out.println(code);
+//        System.out.println(code!="abcde");
+        if(!code.equals("abcde")){
+            response.sendRedirect("http://localhost:8080/myBook_02/pages/user/regist.html");
+            System.out.println("验证码不对");
+        }else {
+            if (userService.existUsername(username)){
+                response.sendRedirect("http://localhost:8080/myBook_02/pages/user/regist.html");
+                System.out.println("用户名存在");
+            }else {
+                userService.registUser(new User(null,username,password,email));
+                System.out.println("注册成功");
+                response.sendRedirect("http://localhost:8080/myBook_02/pages/user/regist_success.html");
+            }
+        }
+
+
+
 
 
 
